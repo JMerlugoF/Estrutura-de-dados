b47ccd1049bb75d1be1d29c15c3942028d83d912
2110103004 <joaomerlugo.aluno@unipampa.edu.br>
2022-05-12 16:49:39 -0300

    atividade 3 e 4

---
+++ atv3/Main.java
@@ -0,0 +1,26 @@
+public class Main{
+    public static void main(String[] args) {
+        
+        Cadeira matematica = new Cadeira();
+        Aluno _12345 = new Aluno("Joao", 19);
+        Aluno _12346 = new Aluno("Jose", 20);
+        Aluno _12347 = new Aluno("Maria", 17);
+        Aluno _12348 = new Aluno("Eduarda", 18);
+
+        matematica.adicionar(_12345);
+        matematica.adicionar(_12346);
+        matematica.adicionar(_12347);
+        matematica.adicionar(_12348);
+
+        System.out.println("\n"+matematica.tamanho());
+
+        System.out.println("\n"+matematica.contem("Joao"));
+        System.out.println("\n"+matematica.contem("Roberto"));
+
+        System.out.println("\n"+matematica.cheio());
+
+        matematica.remove("Jose");
+        matematica.remove("Maria");
+
+    }
+}
